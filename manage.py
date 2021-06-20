d4b8d36da295f53a6bf939c0959700225e599022
changawa-antony <antony.changawa@student.moringaschool.com>
2021-06-21 00:54:33 +0300

    added new module to the manege file

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User, Role
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
-
 manager.add_command('server',Server)
 
 @manager.command
@@ -14,6 +14,10 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Role=Role )
 
 
 if __name__ == '__main__':
