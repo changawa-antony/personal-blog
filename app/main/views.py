e0c5298fe33aec337f5736672ffdce5bcc868463
changawa-antony <antony.changawa@student.moringaschool.com>
2021-06-21 11:25:40 +0300

    Added forms, templates and views

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,36 @@
+from flask.helpers import url_for
+from werkzeug.utils import redirect
+from app.models import Post
 from . import main
-from flask import app, render_template
+from flask import app, render_template,request
 from .forms import CreatePost
 from app.main import forms
 from ..logic import get_quotes
+from flask_login import login_required
+from ..auth import views
+from ..models import Quote, db 
+from ..auth import views
 
+posts = Post
+quote = Quote
 
 @main.route('/')
 def index ():
+      
   quote = get_quotes()
-  return render_template('index.html',quote=quote)
+  show_posts = Post.get_posts()
+  return render_template('index.html',quote=quote, show_post = show_posts)
 
 @main.route('/create-post', methods = ['GET', 'POST'])
+@login_required
 def addpost():
+    login_form = views
     title = 'post'
     form = CreatePost()
-    return render_template('post.html',form = form,title = title)
+
+    if request.method == 'POST':
+        post = Post(title = form.title.data, content = form.content.data)
+        post.save_post()
+        return redirect(url_for('index.html'))
+        
+    return render_template('post.html',form = form,title = title, login_form = login_form)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,11 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Quote:
     '''
@@ -11,12 +18,26 @@ class Quote:
 
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
+    
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
 
     def __repr__(self):
         return f'User {self.username}'
@@ -34,3 +55,30 @@ class Role(db.Model):
     def __repr__(self):
         return f'User {self.name}'
 
+
+class Post:
+    
+    all_posts = []
+
+    def __init__(self,title,content):
+        self.title = title
+        self.content = content
+    
+
+    def save_post(self):
+        Post.all_posts.append(self)
+
+
+    @classmethod
+    def clear_posts(cls):
+        Post.all_posts.clear()
+
+    @classmethod
+    def get_posts(cls):
+
+        response = []
+
+        for post in cls.all_posts:
+            response.append(post)
+
+        return response
