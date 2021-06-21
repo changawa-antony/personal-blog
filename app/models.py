4fdc7e9cf5657be47a5e819bfcad9e3bd07ea8e5
changawa-antony <antony.changawa@student.moringaschool.com>
2021-06-21 22:05:41 +0300

    added user model for the database

--- app/models.py
+++ app/models.py
@@ -18,42 +18,42 @@ class Quote:
 
 
 
-class User(UserMixin,db.Model):
-    __tablename__ = 'users'
+# class User(UserMixin,db.Model):
+#     __tablename__ = 'users'
     
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.String(255))
+#     id = db.Column(db.Integer,primary_key = True)
+#     username = db.Column(db.String(255),index = True)
+#     email = db.Column(db.String(255),unique = True,index = True)
+#     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+#     password_hash = db.Column(db.String(255))
 
-    @property
-    def password(self):
-        raise AttributeError('You cannot read the password attribute')
+#     @property
+#     def password(self):
+#         raise AttributeError('You cannot read the password attribute')
 
-    @password.setter
-    def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+#     @password.setter
+#     def password(self, password):
+#         self.pass_secure = generate_password_hash(password)
 
-    def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
-
-
-    def __repr__(self):
-        return f'User {self.username}'
+#     def verify_password(self,password):
+#         return check_password_hash(self.pass_secure,password)
 
 
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
+#     def __repr__(self):
+#         return f'User {self.username}'
 
 
-    def __repr__(self):
-        return f'User {self.name}'
+
+# class Role(db.Model):
+#     __tablename__ = 'roles'
+
+#     id = db.Column(db.Integer,primary_key = True)
+#     name = db.Column(db.String(255))
+#     users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+#     def __repr__(self):
+#         return f'User {self.name}'
 
 
 class Post:
@@ -82,3 +82,66 @@ class Post:
             response.append(post)
 
         return response
+
+"""Database models."""
+from . import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(UserMixin, db.Model):
+    """User account model."""
+
+    __tablename__ = 'flasklogin-users'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    name = db.Column(
+        db.String(100),
+        nullable=False,
+        unique=False
+    )
+    email = db.Column(
+        db.String(40),
+        unique=True,
+        nullable=False
+    )
+    password = db.Column(
+        db.String(200),
+        primary_key=False,
+        unique=False,
+        nullable=False
+	)
+    website = db.Column(
+        db.String(60),
+        index=False,
+        unique=False,
+        nullable=True
+	)
+    created_on = db.Column(
+        db.DateTime,
+        index=False,
+        unique=False,
+        nullable=True
+    )
+    last_login = db.Column(
+        db.DateTime,
+        index=False,
+        unique=False,
+        nullable=True
+    )
+
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(
+            password,
+            method='sha256'
+        )
+
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
