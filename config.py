735aaebf4ac133d1487720d638e8df86733044e2
changawa-antony <antony.changawa@student.moringaschool.com>
2021-06-23 12:05:29 +0300

    initial commit

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:my-db-01@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgres://furcfmnbpmlmrs:a000a7abd4074c3d6a1ab3ec9d0b016e4e354cd33cdc03a03c2d3b6a2a093095@ec2-52-5-247-46.compute-1.amazonaws.com:5432/d1kg7e04599n7s'
     
     
 class ProdConfig(Config):
